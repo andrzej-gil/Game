1683b4b41c34c14fdb5ad698e04ebadaf74b8517
Andrzej Gil <andrzej.gil@adcubum.com>
2019-07-09 10:17:10 +0200

    t titel und Hauptbeschreibung

---
+++ test/game/GameTest.java
@@ -0,0 +1,26 @@
+package game;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+@Test
+void testName() throws Exception {
+	//given
+	String standortTitel = "Eingang Höllgrotte in Baar";
+	String standortHauptbeschreibung = "Du stehst vor der Höllgrotte in Baar";
+
+	//when
+	String gameOut = "Eingang Höllgrotte in Baar, Du stehst vor der Höllgrotte in Baar";
+	
+	//then
+	assertThat(gameOut, allOf(containsString(standortTitel), containsString(standortHauptbeschreibung)));
+	
+}
+}
